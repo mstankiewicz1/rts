7440bc65cc2a380be60c6640b8e68114be92052d
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2021-10-07 13:46:58 +0200

    finish app traine

---
+++ rts/src/props/Child.tsx
@@ -0,0 +1,13 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+};
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+      <div>
+        {color}
+        <button onClick={onClick}>Click me!</button>
+     </div>
+  )
+};
